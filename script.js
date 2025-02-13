f9c37ebeab02f7a47ccb919d49cea87ca0d97251
Darpan Kambli <darpankambli006@gmail.com>
2025-02-13 12:14:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const maxHeigt = window.innerHeight - btnHeight;
 window.addEventListener('DOMContentLoaded', function(){
     const btn = document.getElementById('noBtn');
 
-    btn.addEventListener('click', ()=>{
+    btn.addEventListener('mouseenter', ()=>{
 
         btn.style.left = Math.floor(Math.random() * (maxWidth + 1)) + 'px';
         btn.style.top = Math.floor(Math.random() * (maxHeigt + 1)) + 'px';
